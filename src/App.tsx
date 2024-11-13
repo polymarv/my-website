74283153ec967af9b4534962b7b4bfc302179fbe
polymarv <marvin-berglen@gmx.de>
2024-11-13 20:30:56 +0100

    removed boilerplate + added github logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import GithubLogo from './assets/github.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+      <div className='flex flex-row items-center gap-4'>
+        <h1 className='order-1'>This site was made with</h1>
+        <a href="https://react.dev" target="_blank" className='order-2'>
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='flex justify-center'>
+        <a href="https://github.com/polymarv" target="_blank">
+          <img src={GithubLogo} className="logo" alt="Github Logo" />
+        </a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
